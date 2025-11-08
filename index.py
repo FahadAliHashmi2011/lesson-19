2079a5228fe9d3057f5f21e58ca1d87a526da9b6
FahadAliHashmi2011 <faiezali2006@gmail>
2025-11-08 15:36:40 +0000

    s

--- index.py
+++ index.py
@@ -58,3 +58,33 @@ for key in test_dict:
         res = res + 1
 
 print("Frequency of K is : " + str(res))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#-------------------------------------------------------------------------------------------------------------------
+country_code = {'India' : '0091',
+                    'Australia' : '0025',
+                    'Nepal' : '00977'}
+
+
+print("Country code for India -")
+print(country_code.get('India', 'Not Found'))
+
+print("Country code for Japan -")
+print(country_code.get('Japan', 'Not Found'))
