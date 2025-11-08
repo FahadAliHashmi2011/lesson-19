4fc9c3aff1cfc5b1ee7b3ddd5dd55580c7318434
FahadAliHashmi2011 <faiezali2006@gmail>
2025-11-08 15:32:03 +0000

    s

--- index.py
+++ index.py
@@ -20,6 +20,8 @@ student_data = {'id1':
     },
 }
 
+
+
 result = {}
 
 for key,value in student_data.items():
@@ -27,3 +29,32 @@ for key,value in student_data.items():
         result[key] = value
 
 print(result)
+
+
+
+
+
+
+
+
+
+
+
+ 
+# ---------------------------------------------------------------------------------------------------------------------------
+test_dict = {'Codingal': 2, 'is': 2, 'best': 2, 'for': 2, 'Coding':
+1}
+
+
+print("The original dictionary : " + str(test_dict))
+
+
+K = 2
+
+
+res = 0
+for key in test_dict:
+    if test_dict[key] == K:
+        res = res + 1
+
+print("Frequency of K is : " + str(res))
